a6f5120ab31fce556817b9387ce791e538e05ef9
SimonHe <simonhe1@mail.com>
2020-06-08 22:55:00 -0400

    Added tip toggler in settings screen

--- app/components/Tip.jsx
+++ app/components/Tip.jsx
@@ -1,27 +1,47 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Slider, Text } from "react-native";
+import React from "react";
+import { View, StyleSheet, Slider, Text, Switch } from "react-native";
 import { connect } from "react-redux";
 import { changeTip, toggleTip } from "../actions/tips";
 
-const Tip = ({ tip, changeTip, toggleTip }) => {
+const Tip = ({ tip, toggled, changeTip, toggleTip }) => {
   return (
     <View style={styles.tipContainer}>
-      <Slider
-        style={styles.slider}
-        minimumValue={15}
-        maximumValue={25}
-        step={1}
-        value={tip}
-        onValueChange={(value) => changeTip(value)}
-      />
-      <Text>Tip: {tip}%</Text>
+      <View style={styles.sliderContainer}>
+        <Slider
+          minimumValue={15}
+          maximumValue={25}
+          step={1}
+          value={tip}
+          onValueChange={(value) => changeTip(value)}
+          disabled={!toggled}
+        />
+        <Text>Tip: {toggled ? tip : 0}%</Text>
+      </View>
+      <View style={styles.toggleContainer}>
+        <Switch value={toggled} onValueChange={() => toggleTip()} />
+        <Text>{toggled ? "on" : "off"}</Text>
+      </View>
     </View>
   );
 };
 const styles = StyleSheet.create({
+  functionsContainer: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  textContainer: {
+    flex: 1,
+  },
   tipContainer: {
     flex: 3,
     padding: 16,
+    flexDirection: "row",
+  },
+  sliderContainer: {
+    flex: 10,
+  },
+  toggleContainer: {
+    flex: 1,
   },
 });
 const mapStateToProps = (state) => {
